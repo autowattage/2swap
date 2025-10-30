def6d034bfa84cfa9d3139eabbca2fca243cd0ba
bunnyguy <pipandw@gmail.com>
2025-10-29 19:08:28 -0500

    button test boilerplate code

--- tvhead/software/buttons.py
+++ tvhead/software/buttons.py
@@ -2,18 +2,24 @@ import gpiozero
 from gpiozero import Button
 import time
 
-buttons = [Button(0), Button(1), Button(1), Button(1)]
+#buttons = [Button(27), Button(22), Button(23), Button(17)]
 active_buttons = [False, False, False, False]
 
 def update_face():
-    global active_buttons, buttons
-    for index, button in enumerate(buttons):
-        if button.is_pressed:
-            active_buttons[index] = not active_buttons[index]
-        if active_buttons[index]:
-            print("Enabled button ", index)
-        else:
-            print("Disabled button ", index)
+    yay = Button(17)
+    if yay.is_pressed:
+        print("Pressed")
+    else:
+        print("Not pressed")
+#def update_face():
+#    global active_buttons, buttons
+#    for index, button in enumerate(buttons):
+#        if button.is_pressed:
+#            active_buttons[index] = not active_buttons[index]
+#        if active_buttons[index]:
+#            print("Enabled button ", index)
+#        else:
+#            print("Disabled button ", index)
 
 def active():
     return any(active_buttons)
